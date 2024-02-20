801e8a9b2d03b272a8353d69c01ca2d3c634f8bd
jdewbury <Jdewbury@uwaterloo.ca>
2024-02-20 12:19:53 -0500

    Update dataset

--- dataset.py
+++ dataset.py
@@ -18,27 +18,27 @@ class CancerNetPCa(Dataset):
         def __getitem__(self, idx):   
             img_path = self.img_path[idx]
             img = nib_to_numpy(img_path)
+            prostate_path, lesion_path = self.mask_path
+            prostate = np.load(prostate_path[idx])
+            lesion = np.load(lesion_path[idx])
             
             if len(self.mask_path.shape) > 1:
-                prostate_path, lesion_path = self.mask_path
-                prostate = np.load(prostate_path[idx])
-                lesion = np.load(lesion_path[idx])
-                prostate_mask = prostate * lesion
-                # align prostate mask with image
-                mask_t= np.transpose(prostate_mask, (2, 1, 0))
-                mask = np.flip(mask_t, axis=1)
-            else:
-                mask_path = self.mask_path[idx]
-                mask = nib_to_numpy(mask_path)
+                lesion *= prostate
+            # align prostate mask with image
+            mask_t= np.transpose(lesion, (2, 1, 0))
+            mask = np.flip(mask_t, axis=1)
 
             img_slice = img[:, :, self.slice]
             mask_slice = mask[:, :, self.slice]
 
-            img_std = resize_image(normalize_image(img_slice), self.img_size)
-            mask_std = resize_image(normalize_image(mask_slice), self.img_size)
+            if self.transform is not None:
+                img_slice = self.transform(img_slice)
+                mask_slice = self.transform(mask_slice)
 
-            img_tensor = torch.from_numpy(img_std).float()
-            mask_tensor = torch.from_numpy(mask_std).long()
+            #img_tensor = torch.from_numpy(img_slice).float()
+            #mask_tensor = torch.from_numpy(mask_slice).long()
 
-            img_tensor = img_tensor.unsqueeze(0)
-            mask_tensor = mask_tensor.unsqueeze(0) 
+            #mg_tensor = img_tensor.unsqueeze(0)
+            #mask_tensor = mask_tensor.unsqueeze(0) 
+
+            return img_slice, mask_slice
